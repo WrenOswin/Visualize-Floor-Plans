b0ee39a9b37b9ee7bf80049c1652291b6530f50c
Wren Oswin <wrenoswin01@gmail.com>
2023-05-09 18:04:11 +0530

    Add files via upload

---
+++ FloorplanToBlenderLib/__init__.py
@@ -0,0 +1,34 @@
+"""
+FloorplanToBlender3d
+
+This file create python package
+
+Example usage of package :
+
+from FloorplanToBlenderLib import * # floorplan to blender lib
+
+detect...
+generate...
+IO...
+transform...
+dialog...
+execution...
+
+"""
+
+__all__ = [
+    "image",
+    "detect",
+    "generate",
+    "IO",
+    "transform",
+    "dialog",
+    "execution",
+    "const",
+    "generator",
+    "draw",
+    "calculate",
+    "config",
+    "stacking",
+    "floorplan",
+]
